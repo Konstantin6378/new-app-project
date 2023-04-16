d1ba6f0b2bbf953a0656164c5d254df4e03f2c3f
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-04-16 13:47:34 +0800

    add
    Wrote a UI - field, button,
    Developed authorization form

--- app/providers/MainProvider.tsx
+++ app/providers/MainProvider.tsx
@@ -1,9 +1,15 @@
 import { FC } from 'react'
 import { QueryClient, QueryClientProvider } from 'react-query'
+import { Provider } from 'react-redux'
 
 import Layout from '@/components/layout/Layout'
 
 import { ReactChild } from '@/config/react'
+
+import { store } from '@/store/store'
+
+import HeadProvider from './HeadProvider/HeadProvider'
+import ReduxToast from './ReduxToast'
 
 const queryClient = new QueryClient({
 	defaultOptions: {
@@ -15,9 +21,14 @@ const queryClient = new QueryClient({
 
 const MainProvider: FC<ReactChild> = ({ children }) => {
 	return (
-		<QueryClientProvider client={queryClient}>
-			<Layout>{children}</Layout>
-		</QueryClientProvider>
+		<HeadProvider>
+			<Provider store={store}>
+				<QueryClientProvider client={queryClient}>
+					<ReduxToast />
+					<Layout>{children}</Layout>
+				</QueryClientProvider>
+			</Provider>
+		</HeadProvider>
 	)
 }
 export default MainProvider
