b9e89f916fe16ce54499a684b70518eaee448f6f
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-04-22 17:33:49 +0800

    add
    Developed a slider,
    Made the logic, through a custom hook

--- app/components/screens/home/home.interface.ts
+++ app/components/screens/home/home.interface.ts
@@ -1,1 +1,5 @@
-export interface IHome {}
+import { ISLide } from '@/ui/slider/slider.interface'
+
+export interface IHome {
+	slides: ISLide[]
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,47 @@
-import { NextPage } from 'next'
+import { errorCatch } from 'api/api.helpers'
+import type { GetStaticProps, NextPage } from 'next'
 
-import Home from '@/components/screens/home/Home'
+import Home from '@/screens/home/Home'
+import { IHome } from '@/screens/home/home.interface'
 
-const HomePage: NextPage = () => {
-	return <Home />
+import { ISlide } from '@/ui/slider/slider.interface'
+
+import { MovieService } from '@/services/movie.service'
+
+import { getGenresList } from '@/utils/movie/getGenresList'
+
+import { getMovieUrl } from '@/config/url.config'
+
+const HomePage: NextPage<IHome> = ({ slides }) => {
+	return <Home slides={slides} />
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+	try {
+		const { data: movies } = await MovieService.getAll()
+
+		const slides: ISlide[] = movies.slice(0, 10).map((m) => ({
+			_id: m._id,
+			link: getMovieUrl(m.slug),
+			subTitle: getGenresList(m.genres),
+			title: m.title,
+			bigPoster: m.bigPoster,
+		}))
+
+		return {
+			props: {
+				slides,
+			} as IHome,
+		}
+	} catch (error) {
+		console.log(errorCatch(error))
+
+		return {
+			props: {
+				slides: [],
+			},
+		}
+	}
 }
 
 export default HomePage
