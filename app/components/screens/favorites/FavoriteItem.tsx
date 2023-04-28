097466191837168dfc0514c6ad6cd8acad723bae
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-04-28 16:06:39 +0800

    add
    FavoritesButton and Styles FavoritesButton

--- app/components/screens/favorites/FavoriteItem.tsx
+++ app/components/screens/favorites/FavoriteItem.tsx
@@ -1,8 +1,10 @@
 import { FC } from 'react'
 
+import { IMovie } from '@/shared/types/movie.types'
+
 import styles from './FavoriteItem.module.scss'
 
-const FavoriteItem: FC = () => {
-	return <div>FavoriteItem</div>
+const FavoriteItem: FC<{ movie: IMovie }> = ({ movie }) => {
+	return <div className={styles.itemWrapper}>FavoriteItem</div>
 }
 export default FavoriteItem
---
+++ app/components/screens/favorites/Favorites.tsx
@@ -0,0 +1,6 @@
+import { FC } from 'react'
+
+const Favorites: FC = () => {
+	return <div>Favorites</div>
+}
+export default Favorites
