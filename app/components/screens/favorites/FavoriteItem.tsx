4dab2582913298a4386a420154aed07bca4efa9e
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-04-28 16:22:56 +0800

    add
    FavoriteItem

--- app/components/screens/favorites/FavoriteItem.tsx
+++ app/components/screens/favorites/FavoriteItem.tsx
@@ -1,10 +1,29 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import { FC } from 'react'
 
 import { IMovie } from '@/shared/types/movie.types'
 
+import { getMovieUrl } from '@/config/url.config'
+
+import FavoriteButton from '../single-movie/FavoriteButton/FavoriteButton'
+
 import styles from './FavoriteItem.module.scss'
 
 const FavoriteItem: FC<{ movie: IMovie }> = ({ movie }) => {
-	return <div className={styles.itemWrapper}>FavoriteItem</div>
+	return (
+		<div className={styles.itemWrapper}>
+			<FavoriteButton movieId={movie._id} />
+			<Link href={getMovieUrl(movie.slug)}>
+				<Image
+					alt={movie.title}
+					src={movie.poster}
+					layout="fill"
+					draggable={false}
+					priority
+				/>
+			</Link>
+		</div>
+	)
 }
 export default FavoriteItem
